622e9d87a307a4647eaedea86cc2fbdbfcb7942a
Deepa Malleeswaran <deepamalleeswaran@Deepas-MBP.fios-router.home>
2020-06-24 22:01:41 -0400

    update our menu-items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import { Route, Link } from 'react-router-dom';
 import './App.css';
 
 import Homepage from './pages/homepage/homepage.component';
 
+const HatsPage = () => (
+  <div>
+    <h1>Hats</h1>
+  </div>
+);
+
 function App() {
   return (
     <div className="App">
-      <Homepage />
+        <Route exact path='/' component={Homepage} />
+        <Route  path='/hats' component={HatsPage} />
     </div>
   );
 }
