ada2c49f8e5c9319ca80b0bbde341d194ee8614a
Deepa Malleeswaran <deepamalleeswaran@Deepas-MBP.fios-router.home>
2020-06-28 10:06:35 -0400

    updating with shop, collection, collection preview

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
 import React from 'react';
-import { Route, Link } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import './App.css';
 
 import Homepage from './pages/homepage/homepage.component';
-
-const HatsPage = () => (
-  <div>
-    <h1>Hats</h1>
-  </div>
-);
+import ShopPage from './pages/shop/shop.components';
 
 function App() {
   return (
     <div className="App">
         <Route exact path='/' component={Homepage} />
-        <Route  path='/hats' component={HatsPage} />
+        <Route  path='/shop' component={ShopPage} />
     </div>
   );
 }
