1998e30d37cd5ed7df646a63dbe611e44c3903ae
Deepa Malleeswaran <deepamalleeswaran@Deepas-MBP.fios-router.home>
2020-07-04 21:18:39 -0400

    added firebase for sign in and signout

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,27 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+
+const config = {
+    apiKey: "AIzaSyARcdK7ZIqwULrXJNnDjriXlqV9j_jJVLI",
+    authDomain: "crown-db-4ca4f.firebaseapp.com",
+    databaseURL: "https://crown-db-4ca4f.firebaseio.com",
+    projectId: "crown-db-4ca4f",
+    storageBucket: "crown-db-4ca4f.appspot.com",
+    messagingSenderId: "469250801552",
+    appId: "1:469250801552:web:1b721915fe0ea516d73d15",
+    measurementId: "G-WDMSC3JB7G"
+  };
+
+  firebase.initializeApp(config);
+
+  export const auth = firebase.auth();
+  export const firestore = firebase.firestore();
+
+  const provider = new firebase.auth.GoogleAuthProvider();
+  provider.setCustomParameters({prompt: 'select_account'});
+
+  export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+  export default firebase;
+  
